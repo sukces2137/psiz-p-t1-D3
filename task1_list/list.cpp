82e43be89722d8fde8c669e6b33bbfa62a3faf8c
Adam-Pacholski <adam.pacholski1985@gmail.com>
2022-04-09 11:36:56 +0200

    Added Definition removeCar

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -23,6 +23,40 @@ void addCar(List* pRoot, char* plateNum)
 
 void removeCar(List* pRoot, char* plateNum)
 {
+	List* temp = pRoot;
+	List* previousTemp = {};
+	List* nextTemp = {};
+
+	bool sPlateNum = false; 
+
+	while (nullptr != temp->pNext)
+	{
+		if (plateNum == temp->pPlateNum) {
+			sPlateNum = true;
+			nextTemp = temp->pNext;
+				break;
+		}
+
+		previousTemp = temp;
+		temp = temp->pNext;
+	}
+
+	if (sPlateNum) {
+		if (nullptr == nextTemp) {
+			previousTemp->pNext = nullptr;
+			delete temp;
+			std::cout << "The element has been removed!" << std::endl;
+		}
+
+		previousTemp->pNext = temp->pNext;
+		delete temp;
+		std::cout << "The element has been removed!" << std::endl;
+
+	}
+	else {
+		std::cout << "Car wasn't found!!" << std::endl;
+	}
+
 
 }
 void printAll(List* pRoot) {
